cb23585bca99dadeb5fabd4fd4d3c1220a62679f
houzeyu2683 <houzeyu2683@gmail.com>
2023-06-07 19:26:24 +0800

    terminal

--- 2204/gnome.sh
+++ 2204/gnome.sh
@@ -1,2 +1,11 @@
-dconf load / < gnome/zero.txt
-dconf load / < gnome/one.txt
+
+dconf load / < gnome/keyboard/zero.txt
+dconf load / < gnome/keyboard/one.txt
+
+link=https://github.com/powerline/fonts.git
+git clone $link --depth=1
+./fonts/install.sh
+rm -rf fonts
+dconf load /org/gnome/terminal/ < gnome/terminal.txt
+
+cp gnome/.bashrc ~/
