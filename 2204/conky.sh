589a29ecaf386421359c0ff05aa7bd56d5b8963d
Greg <houzeyu2683@gmail.com>
2023-06-08 21:26:12 +0800

    Update conky.sh

--- 2204/conky.sh
+++ 2204/conky.sh
@@ -1,9 +1,9 @@
 sudo apt install conky -y
 
-mkdir -p ~/.conky/bee
-#mkdir -p ~/.conky/default
-cp -r conky/bee ~/.conky/
-#cp conky/default/conky.conf ~/.conky/default/
+#mkdir -p ~/.conky/bee
+mkdir -p ~/.conky/default
+#cp -r conky/bee ~/.conky/
+cp conky/default/conky.conf ~/.conky/default/
 
 mkdir -p ~/.config/autostart
 desktop=~/.config/autostart/conky.desktop
@@ -11,6 +11,7 @@ touch $desktop
 echo '[Desktop Entry]' >> $desktop
 echo 'Name=Conky' >> $desktop
 echo 'Type=Application' >> $desktop
-echo 'Exec=conky -c /home/'$USER'/.conky/bee/conky.conf' >> $desktop
+#echo 'Exec=conky -c /home/'$USER'/.conky/bee/conky.conf' >> $desktop
+echo 'Exec=conky -c /home/'$USER'/.conky/default/conky.conf' >> $desktop
 
 sudo rm /usr/share/applications/conky.desktop
